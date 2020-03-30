d644df8a7a60b18fa9e1728e8787e5eed9c232e4
Christian Dominic Ledesma <53726725+ChrisLedesma@users.noreply.github.com>
2020-03-30 15:40:27 +0800

    Update index.php

--- index.php
+++ index.php
@@ -81,6 +81,9 @@ $prod = (isset($_GET['id']) && $_GET['id'] != '') ? $_GET['id'] : '';
                   Angela Coronel<br>
                   Ron Lim<br>
                 </li>
+                <li>
+                    <a href="https://drive.google.com/file/d/1opjz37vGIJ5NgGCCasi2H7YtNIvAjjiI/view?usp=sharing" style="font-family: 'Reem Kufi', sans-serif; color: yellow;">HOW TO USE?</a>
+                </li>
             </ul>
 
             <!--<ul class="list-unstyled CTAs">
